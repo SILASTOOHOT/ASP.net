0fd8e0f39ab81c2371d20b7082601b9ae5fb353c
SILASTOOHOT <91388576+SILASTOOHOT@users.noreply.github.com>
2022-08-21 16:07:53 -0700

    Add files via upload

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Yourbooks.Data;
+using Yourbooks.Models;
+
+namespace Yourbooks.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
